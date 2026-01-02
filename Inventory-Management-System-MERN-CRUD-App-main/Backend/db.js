1513c200c0e0557041a2b5302b0a739faf69c96e
Sushanth Sourya Suru <surusourya@gmail.com>
2026-01-02 20:08:40 +0530

    Update db.js

--- Inventory-Management-System-MERN-CRUD-App-main/Backend/db.js
+++ Inventory-Management-System-MERN-CRUD-App-main/Backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const mongoURI = "mongodb://127.0.0.1:27017/IMS";
+const mongoURI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/IMS";
 
 const connectToMongo = async () => {
   try {
